449a896fc74d47560e27884029fc63d47d79a023
omfg74 <podsh74@gmail.com>
2021-09-14 21:33:19 +0500

    first

---
+++ src/main/java/com/omfgdevelop/jiratelegrambot/JiraTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.jiratelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omfgdevelop/jiratelegrambot/JiraTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.jiratelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
